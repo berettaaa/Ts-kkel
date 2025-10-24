56867bb770ba69b608a02181584a1ae181a0c9dc
berettaaa <mrtnnairis@gmail.com>
2025-10-24 10:17:05 +0300

    Update ülesanne.py

---
+++ ülesanne.py
@@ -0,0 +1,15 @@
+print("1. 5x5 tärnid:")
+for i in range(5):
+print("*" * 5)
+
+print()
+
+print("2. Kasvav kolmnurk:")
+for i in range(1, 6):
+print("*" * i)
+
+print()
+
+print("3. Kahanev kolmnurk:")
+for i in range(5, 0, -1):
+print("*" * i)
